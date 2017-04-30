0a8bd9e5ee67d75cd0419d4f95e5e5cd834632e7
egultekin <emre.gultekin@gmail.com>
2017-04-30 09:12:26 +0300

    fixed XMLEncoding

--- cci/XMLEncoding.java
+++ cci/XMLEncoding.java
@@ -48,28 +48,30 @@ class XMLEncoding {
 			else {
 				tagMapping.put(firstKey, tag);
 				result.append(firstKey++);
-			}
-			Iterator<Map.Entry<String, String>> attIt = attributes.entrySet().iterator();
-			while (attIt.hasNext()) {
-				result.append(DELIMETER);
-				Map.Entry<String, String> entry = attIt.next();
-				tagMapping.put(firstKey, entry.getKey());
-				result.append(firstKey++).append(DELIMETER).append(entry.getValue());
-			}
-			result.append(DELIMETER).append(END);
-			Iterator<Element> it = children.iterator();
-			while (it.hasNext()) {
-				result.append(DELIMETER);
-				Element next = it.next();
-				String childStr = next.encode(firstKey);
-				result.append(childStr);
-				Iterator<Map.Entry<Integer, String>> tagIt = next.tagMapping.entrySet().iterator();
-				while (tagIt.hasNext()) {
-					Map.Entry<Integer, String> e = tagIt.next();
-					tagMapping.put(e.getKey(), e.getValue());
+
+				Iterator<Map.Entry<String, String>> attIt = attributes.entrySet().iterator();
+				while (attIt.hasNext()) {
+					result.append(DELIMETER);
+					Map.Entry<String, String> entry = attIt.next();
+					tagMapping.put(firstKey, entry.getKey());
+					result.append(firstKey++).append(DELIMETER).append(entry.getValue());
 				}
+				result.append(DELIMETER).append(END);
+				Iterator<Element> it = children.iterator();
+				while (it.hasNext()) {
+					result.append(DELIMETER);
+					Element next = it.next();
+					String childStr = next.encode(firstKey);
+					result.append(childStr);
+					Iterator<Map.Entry<Integer, String>> tagIt = next.tagMapping.entrySet().iterator();
+					while (tagIt.hasNext()) {
+						Map.Entry<Integer, String> e = tagIt.next();
+						tagMapping.put(e.getKey(), e.getValue());
+					}
+				}
+				result.append(DELIMETER).append(END);
 			}
-			result.append(DELIMETER).append(END);
+
 			return result.toString();
 		}
 	}
@@ -85,39 +87,48 @@ class XMLEncoding {
 	public String nextToken() {
 		int start = i;
 		boolean foundToken = false;
-		while (i < xml.length && xml[i] != '>' && (!foundToken || xml[i] != ' ') && (!foundToken || xml[i] != '<')) {
-			if (xml[i] != '<' && xml[i] != '\r' && xml[i] != '\n' && !foundToken) {
+		while (i < xml.length && (!foundToken || xml[i] != '>') && (!foundToken || xml[i] != ' ') && (!foundToken || xml[i] != '<')) {
+			if (xml[i] != '<' && xml[i] != '>' && xml[i] != ' ' && xml[i] != '\r' && xml[i] != '\n' && !foundToken) {
 				start = i;
 				foundToken = true;
 			}
 			i++;
 		}
 
-		if (i < xml.length) i++;
-		return String.valueOf(Arrays.copyOfRange(xml, start, i-1));
+		String result = String.valueOf(Arrays.copyOfRange(xml, start, i));
+		//if (xml[i] != '<') i++;
+		return result;
 	}
 
-	public Element parseElement(String parsed, String parent) {
-		Element tagElement = new Element();
-		String token = (null != parsed) ? parsed : nextToken();
-		String tagEndToken = (null != parent) ? '/' + parent : '/' + token;
-		String tag = token;
-		while (i < xml.length && !token.equals(tagEndToken)) {
-			token = nextToken();
-			//if (null != parent) tag = tag + " " + token;
-			String[] attributePair = null;
-			while (i < xml.length && !token.equals(tagEndToken) && (attributePair = parseAttribute(token)) != null) {
-				tagElement.attributes.put(attributePair[0], attributePair[1]);
-				if (i < xml.length) token = nextToken();
-			}
-			while (i < xml.length && !token.equals(tagEndToken)) {
-				Element child = parseElement(token, tag);
-				tagElement.children.add(child);
-				if (i < xml.length) token = nextToken();
-			}
-		}
-		tagElement.tag = tag;
-		return tagElement;
+//	public Element parseElement(String parsed, String parent) {
+//		Element tagElement = new Element();
+//		String token = (null != parsed) ? parsed : nextToken();
+//		String tagEndToken = (null != parent) ? '/' + parent : '/' + token;
+//		String tag = token;
+//		while (i < xml.length && !token.equals(tagEndToken)) {
+//			token = nextToken();
+//			//if (null != parent) tag = tag + " " + token;
+//			String[] attributePair = null;
+//			while (i < xml.length && !token.equals(tagEndToken) && (attributePair = parseAttribute(token)) != null) {
+//				tagElement.attributes.put(attributePair[0], attributePair[1]);
+//				if (i < xml.length) token = nextToken();
+//			}
+//			while (i < xml.length && !token.equals(tagEndToken)) {
+//				Element child = parseElement(token, tag);
+//				tagElement.children.add(child);
+//				if (i < xml.length) token = nextToken();
+//			}
+//		}
+//		tagElement.tag = tag;
+//		return tagElement;
+//	}
+	
+	public Element parse() {
+		Element topMost = null;
+		if ((topMost = parseTag()) == null) throw new RuntimeException("Invalid input detected.");
+		for (Element child : parseChildren(topMost.tag)) 
+			topMost.children.add(child);
+		return topMost;
 	}
 	
 	public Element parseTag() {
@@ -125,9 +136,16 @@ class XMLEncoding {
 		Element element = new Element();
 		element.tag = nextToken();
 		while (i < xml.length && xml[i] != '>') {
-			String[] ap = parseAttribute(nextToken());
-			element.attributes.put(ap[0], ap[1]);
+			Map<String, String> am = parseAttributes();
+			if (null != am) {
+				Iterator<Map.Entry<String, String>> it = am.entrySet().iterator();
+				while (it.hasNext()) {
+					Map.Entry<String, String> ent = it.next();
+					element.attributes.put(ent.getKey(), ent.getValue());
+				}
+			}
 		}
+		i++;
 		return element;
 	}
 	
@@ -135,29 +153,42 @@ class XMLEncoding {
 		String tagEndToken = '/' + tag;
 		List<Element> res = new Vector<Element>();
 		Element child = null;
-		while ((child = parseTag()) != null && !child.tag.equals(tagEndToken)) res.add(child);
+		while ((child = parseTag()) != null && !child.tag.equals(tagEndToken)) {
+			for (Element grandChild : parseChildren(child.tag)) child.children.add(grandChild);
+			res.add(child);
+		}
 		
 		if (null == child) {
-			child = new Element();
+			
 			String token = nextToken();
-			String value = token;
+			String value = "";
 			while (i < xml.length && !token.equals(tagEndToken)) {
+				if (value.equals("")) value = token;
+				else value = value + " " + token;
 				token = nextToken();
-				value = value + " " + token;
 			}
-			child.tag = value;
-			res.add(child);
+			
+			if (!value.equals("")) {
+				child = new Element();
+				child.tag = value;
+				res.add(child);
+			}
 		}
 		
 		return res;
 	}
 
-	public String[] parseAttribute(String attributeToken) {
-		String[] result = attributeToken.split("=");
-		if (result.length < 2) return null;
-		String value = result[1];
-		if (value.startsWith("\"") && value.endsWith("\"")) result[1] = value.substring(1, value.length()-1);
-		return result;
+	public Map<String, String> parseAttributes() {
+		if (xml[i] == '>') return null;
+		Map<String, String> am = new HashMap<String, String>();
+		while (i < xml.length && xml[i] != '>' && xml[i] != '<') {
+			String attributeToken = nextToken();
+			String[] result = attributeToken.split("=");
+			String value = result[1];
+			if (value.startsWith("\"") && value.endsWith("\"")) result[1] = value.substring(1, value.length()-1);
+			am.put(result[0], value);
+		}
+		return am;
 	}
 	
 	public static void main(String[] args) {
@@ -168,7 +199,8 @@ class XMLEncoding {
 		"<person firstName=\"Gayle\">Some Message</person>" +
 		"</family>";
 		XMLEncoding xmlEnc = new XMLEncoding(xml.toCharArray());
-		Element elem = xmlEnc.parseElement(null, null);
+		//Element elem = xmlEnc.parseElement(null, null);
+		Element elem = xmlEnc.parse();
 		System.out.println(elem.encode(1));
 		for (Map.Entry<Integer, String> ent : elem.tagMapping.entrySet()) {
 			System.out.printf("%d: %s\n", ent.getKey(), ent.getValue());
