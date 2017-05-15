c1604089d33d008f4e7d3ea3fe3d3d94f6354f11
Emre GÜLTEKİN <egultekin@stm.com.tr>
2017-05-15 16:54:25 +0300

    binary search, median

--- leetcode/MedianOfTwoSortedArrays.java
+++ leetcode/MedianOfTwoSortedArrays.java
@@ -1,30 +1,76 @@
 package leetcode;
 
+import java.util.Scanner;
+
 public class MedianOfTwoSortedArrays {
 
-	public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-		int len = nums1.length+nums2.length;
-		int order = (len+1)/2;
-		int i1 = 0, i2 = 0, count = 0, num = Integer.MAX_VALUE;
-		while (count < order) {
-			num = (i1 < nums1.length) ? nums1[i1] : Integer.MAX_VALUE;
-			if (i2 < nums2.length) {
-				if (nums2[i2] < num) { 
-					num = nums2[i2];
-					i2++;
-				} else i1++;
-			} else i1++;
-
-			count++;
+	public static double median(int[] a, int[] b) {
+		int n = a.length;
+		int m = b.length;
+		int alo = 0, ahi = n-1, blo = 0, bhi = m-1;
+		boolean even = (n+m)%2==0;
+		int s = even ? (n+m)/2-1 : (n+m)/2;
+		int ai = (alo+ahi)/2;
+		int bi = s-ai;
+		while (ai < n && bi < m && alo < ahi-1 && blo < bhi-1)
+			if (a[ai] == b[bi]) return a[ai];
+			else if (a[ai] > b[bi]) {
+				ahi = ai;
+				blo = bi;
+				ai = (alo+ahi)/2;
+				bi = s-ai;
+			} else {
+				bhi = bi;
+				alo = ai;
+				bi = (blo+bhi)/2;
+				ai = s-bi;
+			}
+		int n1, n2;
+		if (ai >= n) {
+			n1 = b[s-n];
+			n2 = even ? b[s-n+1] : n1;
+		} else if (bi >= m) {
+			n1 = a[s-m];
+			n2 = even ? b[s-m+1] : n1;
+		} else {		
+			int a1, a2, b1, b2;
+			if (alo == ahi-1) {
+				b2 = b[s-alo]; 
+				b1 = b[s-ahi];
+				a1 = a[alo];
+				a2 = a[ahi];
+			} else {
+				b1 = b[blo];
+				b2 = b[bhi];
+				a2 = a[s-blo];
+				a1 = a[s-bhi];
+			}
+			
+			if (a1 >= b1 && b2 <= a2) {
+				n1 = a1; n2 = b2;
+			} else if (a1 >= b1) {
+				n1 = a1; n2 = a2;
+			} else if (a1 < b1 && a2 <= b2) {
+				n1 = b1; n2 = a2;
+			} else {
+				n1 = b1; n2 = b2;
+			}				
 		}
 		
-		if (num == Integer.MAX_VALUE) return (double) 0;
-		else if (len%2==1) return (double)num;
-		else return ((double)num+Math.min(i1 < nums1.length ? nums1[i1] : Integer.MAX_VALUE, i2 < nums2.length ? nums2[i2] : Integer.MAX_VALUE))/2;
+		return ((double)n1+n2)/2;
 	}
 
 	public static void main(String[] args) {
-		System.out.println(new MedianOfTwoSortedArrays().findMedianSortedArrays(new int[] {2, 4, 5, 6, 8}, new int[] {1, 3, 7}));
+		Scanner sc = new Scanner(System.in);
+		int asize = sc.nextInt();
+		int[] a = new int[asize];
+		for (int i = 0; i < asize; i++) a[i] = sc.nextInt();
+		int bsize = sc.nextInt();
+		int[] b = new int[bsize];
+		for (int i = 0; i < bsize; i++) b[i] = sc.nextInt();
+		sc.close();
+		
+		System.out.println(MedianOfTwoSortedArrays.median(a, b));
 	}
 
 }
