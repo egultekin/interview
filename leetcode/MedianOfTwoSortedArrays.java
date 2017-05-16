5bf4c1db1360301e182999f0c614f2dcbec0d79a
Emre GÜLTEKİN <egultekin@stm.com.tr>
2017-05-16 15:53:51 +0300

    median of sorted arrays

--- leetcode/MedianOfTwoSortedArrays.java
+++ leetcode/MedianOfTwoSortedArrays.java
@@ -4,7 +4,12 @@ import java.util.Scanner;
 
 public class MedianOfTwoSortedArrays {
 
-	public static double median(int[] a, int[] b) {
+	public static double median(int[] arr1, int[] arr2) {
+		int[] a = arr1, b = arr2;
+		if (arr1.length > arr2.length) {
+			a = arr2;
+			b = arr1;
+		}
 		int n = a.length;
 		int m = b.length;
 		int alo = 0, ahi = n-1, blo = 0, bhi = m-1;
@@ -26,37 +31,41 @@ public class MedianOfTwoSortedArrays {
 				ai = s-bi;
 			}
 		int n1, n2;
+		int a1, a2, b1, b2;
 		if (ai >= n) {
 			n1 = b[s-n];
-			n2 = even ? b[s-n+1] : n1;
+			n2 = b[s-n+1];
 		} else if (bi >= m) {
 			n1 = a[s-m];
-			n2 = even ? b[s-m+1] : n1;
-		} else {		
-			int a1, a2, b1, b2;
+			n2 = a[s-m+1];
+		} else if (alo == ahi && blo == bhi) {
+			n1 = a[alo];
+			n2 = b[blo];
+		} else {
 			if (alo == ahi-1) {
-				b2 = b[s-alo]; 
+				b2 = s-alo < m && ahi > alo ? b[s-alo] : Integer.MAX_VALUE; 
 				b1 = b[s-ahi];
 				a1 = a[alo];
 				a2 = a[ahi];
 			} else {
 				b1 = b[blo];
 				b2 = b[bhi];
-				a2 = a[s-blo];
+				a2 = s-blo < n && ahi > alo ? a[s-blo] : Integer.MAX_VALUE;
 				a1 = a[s-bhi];
 			}
 			
-			if (a1 >= b1 && b2 <= a2) {
-				n1 = a1; n2 = b2;
-			} else if (a1 >= b1) {
+			if (a1 >= b1 && a2 <= b2) {
 				n1 = a1; n2 = a2;
-			} else if (a1 < b1 && a2 <= b2) {
-				n1 = b1; n2 = a2;
-			} else {
+			} else if (a1 < b1 && a2 > b2) {
 				n1 = b1; n2 = b2;
-			}				
+			} else if (a1 >= b1) {
+				n1 = Math.min(a1, b2); n2 = Math.max(a1, b2);
+			} else {
+				n1 = Math.min(b1, a2); n2 = Math.max(b1, a2);
+			}			
 		}
 		
+		if (!even) n2 = n1;
 		return ((double)n1+n2)/2;
 	}
 
