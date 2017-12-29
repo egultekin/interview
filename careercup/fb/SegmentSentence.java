f6317350b4e5291385de692279684763c8bd4e54
Emre GÜLTEKİN <egultekin@stm.com.tr>
2017-12-29 18:33:53 +0300

    segment sentence

--- careercup/fb/SegmentSentence.java
+++ careercup/fb/SegmentSentence.java
@@ -5,8 +5,8 @@
 * https://www.careercup.com/question?id=5718859091804160
 */
 
-class SegmentString {
-  public int numberOfCutsRequired(String s, int k) {
+class SegmentSentence {
+  public static int numberOfCutsRequired(String s, int k) {
     int cuts = 0;
     int remains = s.length();
     int digits = 4;
@@ -14,18 +14,29 @@ class SegmentString {
     while (remains > 0 && k >= digits+trial) {
       int floor = 10;
       remains = s.length();
-      for (int i=0; i<=trial; i++) {
+      for (int i=1; i<=trial; i++) {
         if (remains > (k-digits-trial)*(floor-1)) {
           remains -= (k-digits-trial)*(floor-1);
           cuts += floor-1;
           floor *= 10;
         } else {
+          cuts += 1+remains / (k-digits-trial);
           remains = 0;
-          cuts += remains / (k-digits-trial);
         }
       }
     }
 
     return cuts;
+  }
+
+  private static String print(String s, int k, int cuts) {
+    StringBuilder sb = new StringBuilder();
+    
+    return sb.toString();
+  }
+
+  public static void main(String[] args) {
+    String s = "This is a good day.";
+    System.out.printf("Can be cut in %d steps.", numberOfCutsRequired(s, 10));
   }
 }
